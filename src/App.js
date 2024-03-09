ab931468f675b1c304e1f5af3841226fb3b52fca
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-03-09 14:46:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const content = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+  { date: "2020-09-03", views: 200, article: "Article 4" },
+];
 
 function App() {
+  const [sortedContent, setSortedContent] = useState(content);
+
+  const HandleSortByDate = () => {
+    const sortedByDate = [...content].sort(
+      (a, b) => new Date(a.date) - new Date(b.date)
+    );
+    setSortedContent(sortedByDate);
+  };
+
+  const HandleSortByViews = () => {
+    const sortedByViews = [...content].sort((a, b) => b.views - a.views);
+    setSortedContent(sortedByViews);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Date and Views Table</h1>
+      <div>
+        <button onClick={HandleSortByDate}>Sort by Date</button>
+        <button onClick={HandleSortByViews}>Sort by Views</button>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+          <tbody>
+            {sortedContent.map((item, index) => (
+              <tr key={index}>
+                <td>{item.date}</td>
+                <td>{item.views}</td>
+                <td>{item.article}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
